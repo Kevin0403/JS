c730c8ef36a1b76770fcafb2caaa41ef03f77fd5
Kevin Sangani <kevinsangani99247@gmail.com>
2023-09-30 15:20:13 +0530

    data types added

---
+++ Basics/02_data Types.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hellow") // We are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+//tc:39 && mdn  --- documentation
+
+console.log("Kevin")
+
+
+let name = "hitesh"
+let age  = 18
+let isLoggedIn = false
+
+// number => 2 to pawer 53
+// bigint
+// string
+// boolean
+// null => standalone value (representation)
+// undefined => value is not assigned
+// symbol => unique
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
